a46100a8a4edfd8aca00b956f6f34604c536e4a3
Julian Leal <julianleal1997@gmail.com>
2022-07-13 13:07:44 -0500

    Swiper works kinda

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -4,56 +4,49 @@ import ProfileInfo from "./ProfileCard.js"
 function Swiper({ currentUser, profiles, profileCount, setProfileCount }) {
 
     const matchURL = "http://localhost:9292/matches"
-    //const [profilesToSwipe, setProfilesToSwipe] = useState(profileList);
-    
-
-    // useEffect(()=> {
-    //     setProfilesToSwipe(window.localStorage.getItem('profilesToSwipe'))
-    // }, [])
-    // useEffect(()=> {
-    //     window.localStorage.setItem('profilesToSwipe', profilesToSwipe)
-    // }, [profilesToSwipe])
 
     function handleLike() {
         checkForMatch(true)
         setProfileCount(profileCount + 1)
-        //handleSwipe()
     }
     function handleDislike() {
         checkForMatch(false)
         setProfileCount(profileCount + 1)
-        //handleSwipe()
     }
 
-    // function handleSwipe() {
-    //     let newProfiles = [...profilesToSwipe].shift()
-    //     setProfilesToSwipe(newProfiles)
-    // }
-
     async function checkForMatch(bool) {
-        await fetch(`${matchURL}/${currentUser.id}/${profiles[profileCount].id}/check`)
-            .then(response => response.json())
-            .then(async (data) => {
-                console.log(data)
-                await updateMatch(bool)
+        await fetch(`${matchURL}/${currentUser.id}/${profiles[profileCount].id}/${bool}`, {
+            method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    swipe_user: bool
+                })
             })
+            // .then(response => response.json())
+            // .then(async (data) => {
+            //     console.log(data)
+            //     const user = data.id
+            //     console.log(user)
+            //     updateMatch(bool, user)
+            // })
         console.log(bool)
     }
 
-    async function updateMatch(bool) {
-        await fetch(`${matchURL}/${currentUser.id}/${profiles[profileCount].id}/update`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify({
-                swipe_user: bool
-            })
-        })
-            .then(resp => resp.json())
-            .then(data => console.log(data))
-    }
+    // function updateMatch(bool, user) {
+    //     fetch(`${matchURL}/${currentUser.id}/${user}/update`, {
+    //         method: "PATCH",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json",
+    //         },
+    //         body: JSON.stringify({
+    //             swipe_user: bool
+    //         })
+    //     })
+    // }
 
     return (
         <div className="card">
