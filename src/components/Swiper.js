d294089ed1999396c6aadd6f5a65c2704f281950
jgarbero-source <103199704+jgarbero-source@users.noreply.github.com>
2022-07-14 14:54:43 -0500

    Update Swiper.js

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import SwiperCard from "./SwiperCard.js";
 
 function Swiper({ currentUser, profiles, profileCount, setProfileCount, isItTheEnd, setIsItTheEnd }) {
