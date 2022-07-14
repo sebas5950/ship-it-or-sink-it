58a14f43052a37e2b9d93169b57e41e030138fa9
sebas5950 <68822252+sebas5950@users.noreply.github.com>
2022-07-14 16:25:41 -0400

    Update SwiperCard.js

--- src/components/SwiperCard.js
+++ src/components/SwiperCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
