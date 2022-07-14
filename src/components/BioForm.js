6cad5701dc4ad3339253a271e847e8bbfd4f9228
sebas5950 <68822252+sebas5950@users.noreply.github.com>
2022-07-14 16:25:09 -0400

    Update BioForm.js

--- src/components/BioForm.js
+++ src/components/BioForm.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 function BioForm({ bio }) {
